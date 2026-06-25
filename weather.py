5c21194f06b64099bca7c047c7fd07478ba6c045
Waffle-Weiwei <weiweiwwwang@gmail.com>
2026-06-25 11:43:26 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -26,6 +26,9 @@ def get_current_weather(lat, lon):
 current_data = get_current_weather(LATITUDE, LONGITUDE)
 current_temp = current_data["current"]["temperature_2m"]
 current_time = current_data["current"]["time"]
+temp_c = current_temp
+temp_f = round(temp_c * 9/5 + 32, 1)
+
 log_df = pd.DataFrame({
     "date": [current_time.split("T")[0]],
     "time": [current_time],
